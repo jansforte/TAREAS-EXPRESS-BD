2bcbd7a780c0a9f95e26ae788509e4dbbebadacb
jansforte <johan.fuentes01@uceva.edu.co>
2024-12-29 16:17:53 -0500

    documentando los servicios

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -2,6 +2,55 @@ const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUI = require("swagger-ui-express");
 const options = {
 
+    definition:{
+        openapi:"3.0.0",
+        info:{
+            title: "API de gestión de Tareas",
+            version: "1.0.0",
+            description: "API para gestionar tareas en una aplicación express"
+        },
+        servers:[
+            {
+                url: "http://localhost:3000/"
+            }
+        ],
+        components:{
+            schemas:{
+                Task:{
+                    type:"object",
+                    required: ['title','description'],
+                    properties:{
+                        id:{
+                            type:"integer",
+                            example:1
+                        },
+                        title:{
+                            type:"string",
+                            example:"Titulo 1"
+                        },
+                        description:{
+                            type:"string",
+                            example:"descripcion"
+                        }
+                    }
+                },
+                setTask:{
+                    type:"object",
+                    required: ['title','description'],
+                    properties:{
+                        title:{
+                            type:"string",
+                            example:"Titulo 1"
+                        },
+                        description:{
+                            type:"string",
+                            example:"descripcion"
+                        }
+                    }
+                }
+            }
+        }
+    },
     apis:["./src/routes/*.js"]
 }
 
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -20,10 +20,136 @@ const validateTaskId = [
         .toInt()
 ]
 
+/**
+ * @swagger
+ * /tasks:
+ *  get:
+ *   sumary: Obtiene todas las tareas
+ *   responses: 
+ *    200:
+ *     description: Lista de todas las tareas
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *        items: 
+ *         $ref: '#/components/schemas/Task'
+ */
+
 router.get("/",taskController.getAllTasks);
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  get:
+ *   sumary: Obtiene tarea por Id
+ *   parameters:
+ *    - name: id
+ *      in: path
+ *      description: ID de la tarea a retornar
+ *      required: true
+ *      schema:
+ *       type: integer
+ *   responses: 
+ *    200:
+ *     description: Lista de la tarea por Id
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *        items: 
+ *         $ref: '#/components/schemas/Task'
+ *    404:
+ *     description: Tarea no encontrada
+ */
 router.get("/:id",validateTaskId,taskController.getTaksById);
+
+/**
+ * @swagger
+ * /tasks:
+ *  post:
+ *   sumary: Registro de tareas
+ *   description: Registro de tareas en la base de datos
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/setTask'
+ *   responses: 
+ *    200:
+ *     description: Registro de tareas en la base de datos
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *        items: 
+ *         $ref: '#/components/schemas/Task'
+ *    400:
+ *     description: Error de validación
+ */
 router.post("/",validateTask,taskController.createTask);
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  put:
+ *   sumary: Actualizar tareas
+ *   description: Actualización de tareas en la base de datos
+ *   parameters:
+ *    - name: id
+ *      in: path
+ *      description: id de la tarea
+ *      required: true
+ *      schema:
+ *       type: string
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/setTask'
+ *   responses: 
+ *    200:
+ *     description: Actualización de tareas en la base de datos
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *        items: 
+ *         $ref: '#/components/schemas/Task'
+ *    400:
+ *     description: Error de validación
+ *    404:
+ *     description: Tarea no encontrada
+ */
 router.put("/:id",[validateTaskId,validateTask],taskController.updateTask);
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  delete:
+ *   sumary: Eliminar Tarea
+ *   description: Eliminar Tarea
+ *   parameters:
+ *    - name: id
+ *      in: path
+ *      description: id de la tarea
+ *      required: true
+ *      schema:
+ *       type: string
+ *   responses: 
+ *    200:
+ *     description: Tarea eliminada
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *        items: 
+ *         $ref: '#/components/schemas/Task'
+ *    404:
+ *     description: Tarea no encontrada
+ */
 router.delete("/:id",validateTaskId,taskController.deleteTask);
 
 module.exports = router;
